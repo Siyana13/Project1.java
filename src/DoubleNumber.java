b8e5021bced341b730c37274a278b9878a5fe9fe
Siyana <siyana_avramova@abv.bg>
2023-04-09 16:52:36 +0300

    New addition

--- src/DoubleNumber.java
+++ src/DoubleNumber.java
@@ -1,11 +1,20 @@
 import java.util.ArrayList;
 
-public class DoubleNumber
-{
-   private ArrayList<Numbers> numbersArrayList;
+public class DoubleNumber {
+    private ArrayList<Numbers> numbersArrayList;
 
     public DoubleNumber() {
         this.numbersArrayList = new ArrayList<>();
     }
+
+    public ArrayList<Numbers> getNumbersArrayList() {
+        return numbersArrayList;
+    }
+
+    public void addDoubleNumber(Numbers numbers)
+    {
+        this.getNumbersArrayList().add(numbers);
+    }
+
 
 }
